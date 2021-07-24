378943f02be7ad836fa4531fb7004008f06bce1d
DaGev <dgev2k@gmail.com>
2021-07-24 23:21:01 +1000

    movie objected completed

---
+++ 2-movie-object/movieObject.js
@@ -0,0 +1,16 @@
+class Movie {
+    constructor(title, director, genre, releaseYear, rating) {
+        this.title = title;
+        this.director = director;
+        this.genre = genre;
+        this.releaseYear = releaseYear;
+        this.rating = rating;
+    }
+
+    get overview() {
+        console.log(`${this.title}, a ${this.genre} film directed by ${this.director} was released in ${this.releaseYear}. It received a rating of ${this.rating}.`);
+    }
+}
+
+const batman = new Movie("batman", "davit", "superhero", "2008", "9 / 10");
+batman.overview;
